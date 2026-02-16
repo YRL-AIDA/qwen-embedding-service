5d3c6de10ec1f32229f85beb3c56b4f75a0e73c9
kirilltobola <kirilltobola@gmail.com>
2026-02-16 17:36:04 +0800

    add embedder type hint

--- src/embedding/dependencies.py
+++ src/embedding/dependencies.py
@@ -1,5 +1,7 @@
 from fastapi import Request
 
+from src.ml.qwen3_vl_embedding import Qwen3VLEmbedder
 
-def get_model_dependency(request: Request):
+
+def get_model_dependency(request: Request) -> Qwen3VLEmbedder:
     return request.app.state.model
